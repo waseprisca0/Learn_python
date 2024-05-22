d9cf28d7897f55e400207a083f1be8d217c10d8e
waseprisca0 <117096121+waseprisca0@users.noreply.github.com>
2024-05-22 19:25:51 +0200

    Update data_types_list.py

--- variables/data_types_list.py
+++ variables/data_types_list.py
@@ -4,3 +4,28 @@ dataTypes = ["int", "float", "complex", "str", "list" , "tuple" , "range" , "set
 # Print out each data type
 for dataType in dataTypes:
     print(dataType)
+#examples of each data type
+int_var = 42
+float_var = 3.14
+complex_var = 1 + 2j
+str_var = "hello"
+list_var = [1, 2, 3]
+tuple_var = (1, 2, 3)
+range_var = range(5)
+set_var = {1, 2, 3}
+dict_var = {"key": "value"}
+bool_var = True
+none_var = None
+
+print(type(int_var))
+print(type(float_var))
+print(type(complex_var))
+print(type(str_var))
+print(type(list_var))
+print(type(tuple_var))
+print(type(range_var))
+print(type(set_var))
+print(type(dict_var))
+print(type(bool_var))
+print(type(none_var))
+
