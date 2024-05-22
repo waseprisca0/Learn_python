2d8025abd07d0f6ef53ad1ceb9a3ee2977d61a7b
waseprisca0 <117096121+waseprisca0@users.noreply.github.com>
2024-05-22 13:31:08 +0200

    Update data_types_list.py

--- variables/data_types_list.py
+++ variables/data_types_list.py
@@ -1,2 +1,7 @@
-int,float
+# List of different data types in python
+data types = ["int", "float", "complex", "str", "list" , "tuple" , "range" , "set" , "dict" , "Boolean" , "Binary" , "None"]
+
+# Print out each data type
+for data type in data types:
+    print(data type)
 
