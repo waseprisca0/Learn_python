4f27927f23fb07ca56e64b4e016ea533efd2e706
waseprisca0 <117096121+waseprisca0@users.noreply.github.com>
2024-05-30 14:48:21 +0200

    Update calculator.py

--- variables/calculator.py
+++ variables/calculator.py
@@ -1,1 +1,51 @@
+# Functions for Operations
 
+def add(x, y):
+    
+    return x + y
+
+def subtract(x, y):
+   
+    return x - y
+
+def multiply(x, y):
+    
+    return x * y
+
+def divide(x, y):
+   
+    if y == 0:
+        raise ValueError("Can not be divided by zero!")
+    else:
+        return x / y
+
+# Calculate Function
+
+def calculate(operation, x, y):
+    
+    if operation == '+':
+        return x+y
+    elif operation == '-':
+        return x-y
+    elif operation == '*':
+        return x*y
+    elif operation == '/':
+        return divide(x, y)
+    else:
+        raise ValueError("Invalid operation!")
+
+# Main Function
+
+def main():
+    try:
+        print("Addition:", calculate('+', 10, 5))             
+        print("Subtraction:", calculate('-', 10, 5))     
+        print("Multiplication:", calculate('*', 10, 5))  
+        print("Division:", calculate('/', 10, 5))          
+        print("Division:", calculate('/', 10, 0))          
+    except ValueError as e:
+        print(e)
+
+# Run the main function
+if __name__ == "__main__":
+    main()
