92e1c1f67b90cf4a252541c9785c3c1df8422828
waseprisca0 <117096121+waseprisca0@users.noreply.github.com>
2024-06-05 19:37:27 +0200

    Update class.py

--- variables/class.py
+++ variables/class.py
@@ -1,40 +1,40 @@
 class Calculator:
-    def add(self, x, y):
-         return x + y
-
-    def subtract(self, x, y):
-        return x - y
-
-    def multiply(self, x, y):
-        return x * y
-
-    def divide(self, x, y):
+    def add(self,x,y):
+        return x+y
+    def sub(self,x,y):
+        return x-y
+    def mult(self,x,y):
+        return x*y
+    def div(self,x,y):
         if y == 0:
-            raise ValueError("Cannot divide by zero!")
-        return x / y
+            return ("can't be divided")
+        return x/y
+    
 
 def main():
+   
+    x = float(input("enter number 1st:"))
+    y = float(input("enter number 2nd:"))
+    operation = input("choose operation(:x,-,*,/):")
+
     calculator = Calculator()
-    
-    x = float(input("Enter first number: "))
-    y = float(input("Enter second number: "))
-    operation = input("Choose an operation (+, -, *, /): ")
 
     try:
         if operation == '+':
-            result = calculator.add(x, y)
+            result = calculator.add(x,y)
         elif operation == '-':
-            result = calculator.subtract(x, y)
+            result = calculator.sub(x,y)
         elif operation == '*':
-            result = calculator.multiply(x, y)
+            result = calculator.mult(x,y)
         elif operation == '/':
-            result = calculator.divide(x, y)
+            result = calculator.div(x,y)
         else:
-            raise ValueError("Invalid operation!")
+            raise ValueError("invalide operation")
+
+        print(f"{x} {operation} {y} = {result}")
         
-        print(f"The result of {x} {operation} {y} = {result}")
     except ValueError as e:
         print(e)
 
-if __name__ == "__main__":
+if __name__ =="__main__":
     main()
